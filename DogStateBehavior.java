4fae40be297d64b20b68504d8e59f7b0e42f1522
Srini <Srinirao.cloud@gmail.com>
2019-05-07 15:56:20 -0400

    state and behavior relation

---
+++ DogStateBehavior.java
@@ -0,0 +1,11 @@
+class DogStateBehavior{
+int size;
+String name;
+
+void bark()
+{
+if(size>60)
+{
+System.out.println("Woof Woof");
+}else if(size >14){
+System.out.println("ruf ruf");}}}
---
+++ DogTestStateBehavior.java
@@ -0,0 +1,15 @@
+class DogTestStateBehavior{
+
+public static void main(String[] args)
+{
+DogStateBehavior one = new DogStateBehavior();
+one.size = 20;
+DogStateBehavior two = new DogStateBehavior();
+two.size = 60;
+DogStateBehavior three = new DogStateBehavior();
+three.size = 70;
+
+one.bark();
+two.bark();
+three.bark();
+}}
